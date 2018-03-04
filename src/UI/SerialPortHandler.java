2c3305ac65aa4bded0223f727caa8b3cbf63abe3
kawhiy <harry0601@icloud.com>
2018-03-04 22:05:27 +0000

    Satellite method optimization
    Break large methods into small methods on reading NMEA message. Added comments to each methods.

--- src/UI/SerialPortHandler.java
+++ src/UI/SerialPortHandler.java
@@ -1,8 +1,4 @@
-/*
- * @Author harry
- */
 package UI;
-
 
 import gnu.io.*;
 import java.io.IOException;
@@ -16,21 +12,25 @@ import java.awt.Color;
 import java.awt.Font;
 
 /**
- *
- * @author harry
+ * @author Harry Sun (Group L WorkPackage 5)
+ * @last_update 04/03/2018
+ * @function open and read NMEA message from serial port, and display it on Satellite UI
  */
 public class SerialPortHandler implements Runnable, SerialPortEventListener{
+    
     static SerialPort serialPort;
     static InputStream in;
     static Thread readThread;
     static CommPortIdentifier portID;
     static Enumeration portList;
-    static double latitude; //latitude
-    static char direction;
-    static double longitude; //longitude
-    static char dir;
-    static String time;
-    static boolean signal;
+    static double latitude;     //latitude
+    static char   direction;    // direction of latitude
+    static double longitude;    // longitude
+    static char   dir;          // direction of longitude
+    static String time;         // time last update the position
+    static boolean signal;      // indicates whether there is signal
+    
+    /* Open serial port*/
     public SerialPortHandler(){        
        try{
             serialPort = (SerialPort)portID.open("TheSatellite",3000);
@@ -53,15 +53,17 @@ public class SerialPortHandler implements Runnable, SerialPortEventListener{
             readThread = new Thread(this);
             readThread.start();    
      
-    }
-    public void run(){
+ }
+    
+public void run(){
         try{
             Thread.sleep(20000);
         }catch(InterruptedException e){System.out.println(e);};       
-    }
-    
-    @Override
-    public void serialEvent(SerialPortEvent event){
+}
+ 
+/* Read data from serial port, only read the message when there is data available */
+@Override
+public void serialEvent(SerialPortEvent event){
         switch(event.getEventType()){
             case SerialPortEvent.BI:
             case SerialPortEvent.OE:
@@ -77,97 +79,115 @@ public class SerialPortHandler implements Runnable, SerialPortEventListener{
                 break;
         }
   }
-                 
-
+ 
+/* Read from the serial port, convert messages and update position */
 public static void update(){
-        try{
+    try{
         byte[] buffer  = new byte[ 1024 ];
         String s;
-        int    n;
-               
+        int    n;             
         while( ( n = in.read( buffer ) ) > -1 ) {
-       // n = in.read(buffer);
         s = new String( buffer, 0, n ); 
-        //System.out.println(s);
-        if (s.startsWith("$GPGLL,,,")){
-            displayNoSignal();
-            signal = false;
-        }
-        else if (s.startsWith("$GPGLL")){
-            //System.out.println(s);
-            signal = true;
-            Integer degrees  = Integer.valueOf(s.substring(7,9)); //latitude degrees
-            Integer minites  = Integer.valueOf(s.substring(9,11));//latitude minutes		  
-            double seconds  = 60*0.00001*(Integer.valueOf(s.substring(12,17))); //latitude seconds, returned 
-                                                                                 //string is in the form of 0.xxxxx mins 
-                                                                                //therefore *60*0.00001 to convert to seconds 
-            direction= s.charAt(18); // direnction of latitude 				   
-            latitude = degrees + minites/60.0 + seconds / 3600.0;
-            if (direction=='S')
-                latitude = (-1) * latitude; // convert latitude to nagetive if direction is South 
-            
-            Integer deg  = Integer.valueOf(s.substring(20,23)); //longitude degrees
-            Integer min  = Integer.valueOf(s.substring(23,25));//longitude minutes		  
-            double sec  = 60*0.00001*(Integer.valueOf(s.substring(26,31))); //longitude seconds, returned 
-									 //string is in the form of 0.xxxxx mins 
-									//therefore *60*0.00001 to convert to seconds 
-            dir = s.charAt(32); // direnction of longitude			   
-            longitude = deg + min/60.0 + sec / 3600.0;
-
-            if (dir=='W')
-                longitude = (-1) * longitude; // convert longitude to nagetive if direction is West 
-           
-             time     = (((s.substring(34,36).concat(":")).
-                        concat(s.substring(36,38))).
-                        concat(":")).concat(s.substring(38,40));//time 
-            XtrekUI.Time.setText(time);
-            XtrekUI.Time.setForeground(Color.green);
-            displayPosition();  // display the updated postion to UI screen
-        }
-        }
-        }catch(IOException e){System.out.println(e);}
-       
         
-    }
+        // no available message.. 
+        if (s.startsWith("$GPGLL,,,")){
+            signal = false;
+            displayNoSignal();
+        }
+        // available message..
+        else if (s.startsWith("$GPGLL")){
+            signal = true;
+            direction= s.charAt(18);      // direnction of latitude 	
+            dir      = s.charAt(32);      // direnction of longitude
+            /* Conver NMEA message */
+            latitude  = converter(s.substring(7,9),s.substring(9,11),s.substring(12,17));
+            longitude = converter(s.substring(20,23),s.substring(23,25),s.substring(26,31));
+            time     =  convertTime(s.substring(34,36),s.substring(36,38),s.substring(38,40));
+            displayTime();
+            displayPosition();  
+            }
+        }
+   }catch(IOException e){System.out.println(e);}      
+}
 
-public static double round(double value, int places) {
+/* Convert NMEA string message into decimal degrees */
+private static double converter(String degree, String minute, String second){
+    Integer degrees  = Integer.valueOf(degree);
+    Integer minites  = Integer.valueOf(minute);
+    double seconds  = 60*0.00001*(Integer.valueOf(second));
+    double posi = degrees + minites/60.0 + seconds / 3600.0;
+    if (direction=='S') 
+        posi = (-1) * posi;
+    if (dir=='W')
+        posi = (-1) * posi;
+    return posi;
+}
+
+/* Convert NMEA string message into time*/
+private static String convertTime(String hour, String minute, String second){
+    return ((((hour.concat(":")).concat(minute)).concat(":")).concat(second));
+}
+
+/* Round a value to certain decimal places */
+private static double round(double value, int places) {
     if (places < 0) throw new IllegalArgumentException();
 
     BigDecimal bd = new BigDecimal(value);
     bd = bd.setScale(places, RoundingMode.HALF_UP);
     return bd.doubleValue();
 }
+
+/* Display no signal in the UI */
 private static void displayNoSignal(){
-    XtrekUI.Latitude.setText("      No signal!");
-    XtrekUI.Latitude.setForeground(Color.red);
-    XtrekUI.Longitude.setText("      No signal!"); 
-    XtrekUI.Longitude.setForeground(Color.red);
+    if (signal == false){
+        XtrekUI.Latitude.setText("      No signal!");
+        XtrekUI.Latitude.setForeground(Color.red);
+        XtrekUI.Longitude.setText("      No signal!"); 
+        XtrekUI.Longitude.setForeground(Color.red);
+    }
 }
 
+/* Display the newly read position*/
 private static void displayPosition(){
-    XtrekUI.Latitude.setText("Latitude: "+round(Math.abs(latitude),4)+" , "+direction);
-    Font f = new Font("MS Reference San Serif",Font.BOLD,18);
-    XtrekUI.Latitude.setFont(f);
-    XtrekUI.Latitude.setForeground(Color.green);
-    XtrekUI.Longitude.setText("Longitude: "+round(Math.abs(longitude),4)+" , "+dir);
-    XtrekUI.Longitude.setForeground(Color.green);
-    XtrekUI.Longitude.setFont(f);
+    if (signal == true){
+        XtrekUI.Latitude.setText("Latitude: "+round(Math.abs(latitude),4)+" , "+direction);
+        Font f = new Font("MS Reference San Serif",Font.BOLD,18);
+        XtrekUI.Latitude.setFont(f);
+        XtrekUI.Latitude.setForeground(Color.green);
+        XtrekUI.Longitude.setText("Longitude: "+round(Math.abs(longitude),4)+" , "+dir);
+        XtrekUI.Longitude.setForeground(Color.green);
+        XtrekUI.Longitude.setFont(f);
+    }
 }
 
+/* Display time*/
+private static void displayTime(){
+    XtrekUI.Time.setText(time);
+    XtrekUI.Time.setForeground(Color.green);
+}
 
+/* Public method, return latitude */
 public static double getLatitude(){
-        return (latitude); // get latitude withour direction 
-    }
+    return (latitude); 
+}
+
+/* Public method, return longitude */
 public static double getLongitude(){  
-        return(longitude); // get longitude without direction 
-    }
+        return(longitude); 
+}
+
+/* Public method, return position containing latitude and longitude */
 public static String getPosition(){
     String position = "" + latitude + ","+longitude;
     return position;
 }
+
+/* Public method, return time*/
 public static String getTime(){
         return(time);
-    }
+}
+
+/* Disconnect the port */
 public void disconnect(){
     try{
         serialPort.removeEventListener();
@@ -175,8 +195,9 @@ public void disconnect(){
         in.close();
         signal = false;
     }catch(Exception e){}               
-    }
+}
 
+/* Public method, to connect port and try to get GPS location */
 public static void connect(){
       portList = CommPortIdentifier.getPortIdentifiers();
       
@@ -190,6 +211,7 @@ public static void connect(){
       }
   }
     
+
 }       
         
     
